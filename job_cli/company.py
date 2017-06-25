6895dbbf5a7622203a2a828dc0ebb815872172e3
Kenny Gao <kgao9@wisc.edu>
2017-06-24 23:42:14 -0500

    added found field for how we found the company

--- job_cli/company.py
+++ job_cli/company.py
@@ -1,13 +1,22 @@
 from job_cli.description_object import DescriptionObject
 
 class Company(DescriptionObject):
-    def __init__(self, fileObj, name, link, business_type):
+    def __init__(self, fileObj, name, link, business_type, found):
         DescriptionObject.__init__(self, fileObj, name)
         self.link = link
         self.business_type = business_type
+
+        #how I heard about said company
+        #should technically be in description object
+        #but then I'd need to think of a better name than description
+        #object and I just can't... I SUCK :'(
+        self.found = found
 
     def get_link(self):
         return self.link
 
     def get_business_type(self):
         return self.business_type
+
+    def get_found(self):
+        return self.found
--- tests/test_company.py
+++ tests/test_company.py
@@ -9,12 +9,14 @@ class TestVim(unittest.TestCase):
         name = "Facebook"
         link = 'https://www.facebook.com/pg/facebook/about/'
         business_type = 'huge company'
+        found = 'GOOGLE TOLD ME'
        
         #create object
-        facebook = Company(myFile, name, link, business_type)
+        facebook = Company(myFile, name, link, business_type, found)
 
         self.assertEqual(facebook.get_link(), link)
         self.assertEqual(facebook.get_business_type(), 'huge company')
+        self.assertEqual(facebook.get_found(), 'GOOGLE TOLD ME')
 
 if __name__ == '__main__':     
     unittest.main()
